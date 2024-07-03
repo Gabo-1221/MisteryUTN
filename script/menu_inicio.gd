cadcc627c2c0585d7de5a4f298caee6cee24a002
Gabo-1221 <153243524+Gabo-1221@users.noreply.github.com>
2024-07-03 15:16:01 -0600

    musica de fondo

--- script/menu_inicio.gd
+++ script/menu_inicio.gd
@@ -8,7 +8,9 @@ func _ready():
 	$personaje1/max.play("mantener")
 	$personaje2/sam.play("mantener")
 	$personaje3/steve.play("mantener")
-	
+
+func _process(delta):
+	apa_playerDesc()
 
 func _on_personaje_1_pressed():
 	global.player = player1
@@ -21,3 +23,9 @@ func _on_personaje_2_pressed():
 func _on_personaje_3_pressed():
 	global.player = player3
 	get_tree().change_scene_to_file("res://escenario.tscn")
+
+
+func apa_playerDesc():
+	if Input.is_action_pressed("buttonSecret1") and Input.is_action_pressed("buttonSecret2") and Input.is_action_pressed("buttonSecrect3"):
+		$personaje3.visible = true
+
